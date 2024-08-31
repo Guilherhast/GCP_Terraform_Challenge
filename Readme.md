c9ca232129176cbffe25bb6e58b6b197a4fd44bc
Guilherme Hastenreiter Ferreira <hast.job@gmail.com>
2024-08-31 19:02:42 -0300

    Task 1

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Build Infrastructure with Terraform on Google Cloud: Challenge Lab
+
+
+Challange link: <https://www.cloudskillsboost.google/course_templates/636/labs/464836>
+
+Repository link: <https://github.com/Guilherhast/GCP_Terraform_Challenge>
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "The project ID to host the network in"
+  default     = ""
+}
+variable "region" {
+  description = "Region of the project"
+  default     = ""
+}
+variable "zone" {
+  description = "Zone of the project"
+  default     = ""
+}
